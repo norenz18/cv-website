143b2218973da56a77e13acd7b081f35ecd67805
norenz <norenzmanalang18@gmail.com>
2021-11-02 02:05:17 +0800

    fixed the navigation bar

--- script.js
+++ script.js
@@ -15,19 +15,43 @@ document.querySelector(".arrow").style.opacity = opacity;
 });
 
 
+
 // NAVBAR ANIMATION
-const hamburger = document.querySelector("#hamburger")
-const navbar = document.querySelector("#navbar")
-const links = document.querySelector('#navbar li')
+const hamburger = document.querySelector("#hamburger");
+let navbar = document.querySelector("#navbar");
+const links = document.querySelectorAll('.nav-link');
+const closedIcon = document.querySelector(".close");
+const bars = document.querySelector(".bars");
+let linkens = navbar.querySelectorAll(".nav-link")
 
-hamburger.addEventListener("click", () => {
-  navbar.classList.toggle('show')
-
-  links.forEach(link => {
-      link.classList.toggle('fade');
+for (let i = 0; i < linkens.length; i++) {
+    linkens[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "")
+   
+    this.className += " active"
   })
-    
-})
+}
+
+hamburger.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+
+  if (navbar.classList.contains('show')) {
+    navbar.classList.toggle('show')
+    closedIcon.style.display = "block"
+    bars.style.display = "none"
+   
+   } else {
+    navbar.classList.toggle('show')
+    closedIcon.style.display = "none"
+    bars.style.display = "block"
+  
+   }
+}
+
+links.forEach(link =>link.addEventListener("click", toggleMenu))
+
 
 
 // HEADER ANIMATION
@@ -38,17 +62,17 @@ let scroll = $(window).scrollTop();
 
 if (scroll > 550) {
   $(".header").css("background" , "#022C43");
-$(".header").css("padding" , "25px 0");
+  $(".header").css("padding" , "25px 0");
 }
 else if (scroll > 5) {
-$(".header").css("background" , "rgba(0,0,0,.5)")
-$(".header").css("padding" , "25px 0");
+    $(".header").css("background" , "rgba(0,0,0,.5)")
+    $(".header").css("padding" , "25px 0");
 
 }
 else {
     $(".header").css("background" , "transparent");
-$(".header").css("font-size" , "16px"); 
-$(".header").css("padding" , "50px 0");
+    $(".header").css("font-size" , "16px"); 
+    $(".header").css("padding" , "25px 0");
 
 }
 })
